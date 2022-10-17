d6245ff76c74478d830776696275e3fb63886f47
Pylyp <pylyp.vynnykov@justeattakeaway.com>
2022-10-17 16:53:22 +0200

    add catalog service

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,40 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
+namespace Catalog.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddSingleton<ICatalogContext, CatalogContext>();
+            builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IReadOnlyCollection<Product>> GetProducts()
+        {
+            return await _context.Products
+                .Find(p => true).ToListAsync();
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context.Products
+                .Find(p => p.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<Product> GetProductByName(string name)
+        {
+            return await _context.Products
+                .Find(p => p.Name == name)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyCollection<Product>> GetProductByCategory(string categoryName)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var result = await _context.Products.ReplaceOneAsync(p => p.Id == product.Id, replacement: product);
+            return result.IsAcknowledged && result.ModifiedCount == 1;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            var result = await _context.Products.DeleteOneAsync(p => p.Id == id);
+            return result.IsAcknowledged && result.DeletedCount == 1;
+        }
+    }
+}
